3e3a8270470b4633c8b16b045e50269f04f3f2af
gstbimo <gusti.bimo@mekari.com>
2023-05-19 22:19:56 +0700

    build saluran

---
+++ src/main/java/com/marlaw/App.java
@@ -0,0 +1,20 @@
+package com.marlaw;
+
+import static java.lang.System.out;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String...args) {
+        var myName = "Marla";
+        out.println(myName);
+    }
+}
